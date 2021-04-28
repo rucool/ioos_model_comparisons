7b6782ee40e1aac0337a0da69354c3c7f096e9af
Mike Smith <michaesm@marine.rutgers.edu>
2021-04-28 15:40:38 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='hurricanes',
+    version='',
+    packages=['src', 'scripts', 'scripts.transects', 'scripts.surface_maps', 'current_storms'],
+    url='',
+    license='',
+    author='mikesmith',
+    author_email='michaesm@marine.rutgers.edu',
+    description='Rutgers Hurricane Model Comparisons'
+)
