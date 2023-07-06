14d90cf0e43b4aae150ba8c4fad338ed3bfa929f
Mike Smith <michaesm@marine.rutgers.edu>
2023-07-06 15:08:31 -0400

    Update plotting.py

--- ioos_model_comparisons/plotting.py
+++ ioos_model_comparisons/plotting.py
@@ -184,11 +184,11 @@ def map_add_currents(ax, ds, coarsen=None, ptype="quiver",
     return q
 
 
-def map_add_eez(ax, zorder=1, color='white', linewidth=0.75):
+def map_add_eez(ax, zorder=1, color='white', linewidth=0.75, linestyle='-.'):
     shape_feature = cfeature.ShapelyFeature(
         Reader(conf.eez_path).geometries(), 
         proj['data'],
-        linestyle='-.',
+        linestyle=linestyle,
         linewidth=linewidth,
         edgecolor=color, 
         facecolor='none'
@@ -1460,8 +1460,8 @@ def plot_model_region_comparison_streamplot(ds1, ds2, region,
 
         # Add EEZ
         if eez:
-            eez1 = map_add_eez(ax1, zorder=1, color='red', linewidth=1)
-            eez2 = map_add_eez(ax2, zorder=1, color='red', linewidth=1)
+            eez1 = map_add_eez(ax1, zorder=1, color='red', linewidth=2, linestyle='-')
+            eez2 = map_add_eez(ax2, zorder=1, color='red', linewidth=2, linestyle='-')
 
         if colorbar:
             cb = fig.colorbar(m1, ax=axs[:2], orientation="horizontal", shrink=.95, aspect=80)#, shrink=0.7, aspect=20*0.7)
