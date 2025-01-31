e136d775e2292af9ca2c1b17cdae08355c90ed66
Mike Smith <michaesm@marine.rutgers.edu>
2025-01-30 23:05:10 -0500

    Update calc.py

--- ioos_model_comparisons/calc.py
+++ ioos_model_comparisons/calc.py
@@ -550,3 +550,47 @@ def rotate_vector(u, v, theta):
     ur = u*cos_theta + v*sin_theta
     vr = -u*sin_theta + v*cos_theta
     return ur, vr
+
+
+import matplotlib.colors
+import matplotlib.pyplot as plt
+def categorical_cmap(nc, nsc, cmap="tab10", continuous=False):
+    """
+    Expand your colormap by changing the alpha value (opacity) of each color.
+    
+    Returns a colormap with nc*nsc different colors, 
+    where for each category there are nsc colors of same hue.
+    
+    
+    From ImportanceOfBeingErnest
+    https://stackoverflow.com/a/47232942/2643708
+    
+    Args:
+        nc (int): number of categories (colors)
+        nsc (int): number of subcategories (shades for each color)
+        cmap (str, optional): matplotlib colormap. Defaults to "tab10".
+        continuous (bool, optional): _description_. Defaults to False.
+
+    Raises:
+        ValueError: Too many categories for colormap
+
+    Returns:
+        object: matplotlib colormap
+    """
+
+    if nc > plt.get_cmap(cmap).N:
+        raise ValueError("Too many categories for colormap.")
+    if continuous:
+        ccolors = plt.get_cmap(cmap)(np.linspace(0,1,nc))
+    else:
+        ccolors = plt.get_cmap(cmap)(np.arange(nc, dtype=int))
+    cols = np.zeros((nc*nsc, 3))
+    for i, c in enumerate(ccolors):
+        chsv = matplotlib.colors.rgb_to_hsv(c[:3])
+        arhsv = np.tile(chsv,nsc).reshape(nsc,3)
+        arhsv[:,1] = np.linspace(chsv[1],0.25,nsc)
+        arhsv[:,2] = np.linspace(chsv[2],1,nsc)
+        rgb = matplotlib.colors.hsv_to_rgb(arhsv)
+        cols[i*nsc:(i+1)*nsc,:] = rgb
+    cmap = matplotlib.colors.ListedColormap(cols)
+    return cmap
